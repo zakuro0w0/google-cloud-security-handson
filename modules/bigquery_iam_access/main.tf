0ed54e90cd1c6c2aaa883bddaf9d07540def972e
zakuro0w0 <tatatake0w0@gmail.com>
2024-08-03 16:15:54 +0900

    feat: 2つ目のテーブルを追加

--- modules/bigquery_iam_access/main.tf
+++ modules/bigquery_iam_access/main.tf
@@ -21,3 +21,19 @@ resource "google_bigquery_table" "table_A" {
 ]
 EOF
 }
+
+resource "google_bigquery_table" "table_B" {
+  dataset_id = google_bigquery_dataset.dataset_A.dataset_id
+  table_id   = "table_B"
+  deletion_protection = false
+
+  schema = <<EOF
+[
+  {
+    "name": "fieldname",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  }
+]
+EOF
+}
