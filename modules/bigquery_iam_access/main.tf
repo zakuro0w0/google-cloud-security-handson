05b6f5edf10ded69ab34535d76ebf6eeef5b5c9f
zakuro0w0 <tatatake0w0@gmail.com>
2024-08-03 17:33:05 +0900

    feat: サービスアカウントを作ってBigQueryデータ閲覧者権限を与える

--- modules/bigquery_iam_access/main.tf
+++ modules/bigquery_iam_access/main.tf
@@ -6,6 +6,17 @@ resource "google_bigquery_dataset" "dataset_A" {
     dataset_id = "dataset_A"
 }
 
+resource "google_service_account" "service_account" {
+  account_id   = "bigquery-iam-access"
+  display_name = "BigQuery Service Account"
+}
+
+resource "google_project_iam_member" "bigquery_role" {
+  project = data.google_project.current.project_id
+  role    = "roles/bigquery.dataViewer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
 resource "google_bigquery_table" "table_A" {
   dataset_id = google_bigquery_dataset.dataset_A.dataset_id
   table_id   = "table_A"
